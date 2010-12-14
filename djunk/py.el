4f3e141a3eb58992b9fe5333a99fc78a3509759f
Damion Junk <damion@wujuko.com>
2010-12-13 20:56:30 -0500

    updating for new machine

---
+++ djunk/py.el
@@ -0,0 +1,13 @@
+;;; Electric Pairs
+;;;
+
+;;; bind RET to py-newline-and-indent
+;;; bind RET to py-newline-and-indent
+(add-hook 'python-mode-hook '(lambda ()
+     (define-key python-mode-map "\C-m" 'newline-and-indent)))
+
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+(setq-default py-indent-offset 4)
