7f7c8cda2c728fdac2630fe64c1b7481016bfffa
Damion Junk <damion@wujuko.com>
2011-04-22 14:39:19 -0400

    cleaning up

--- junkda/rb.el
+++
@@ -1,6 +0,0 @@
-;;(require 'ruby-electric)
-;;(add-hook 'ruby-mode-hook (lambda () (ruby-electric-mode t)))
-
-(defun echo-false-comint () (setq comint-process-echoes t))
-
-(add-hook ’comint-mode-hook ’echo-false-comint)
